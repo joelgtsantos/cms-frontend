cdd8b25bcdb8ceb624ccf092df6d2f3bd4e6b35a
Joel Santos <joelgtsantos@gmail.com>
2018-07-06 14:44:45 -0600

    Remove mode variable

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const server = process.env.PORT || 'localhost';
 const port = process.env.PORT || 1234;
 
 app.use('/', express.static(path.join(__dirname, 'build')));
 
-app.listen(port, () => console.log(`listening on ${port}`));
+app.listen(port, server, () => console.log(`listening on ${port}`));
