1067403d18cad1fb50a1cd7ffc3dd7de7aac96d2
Joel Santos <joelgtsantos@gmail.com>
2018-07-06 14:24:36 -0600

    Modify folder dist to build

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 1234;
 
-app.use('/', express.static(path.join(__dirname, 'dist')));
+app.use('/', express.static(path.join(__dirname, 'build')));
 
 app.listen(port, () => console.log(`listening on ${port}`));
